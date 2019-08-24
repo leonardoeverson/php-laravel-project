55b1711c746f935a2d8047317d7bf4ee8316ed34
leonardoeverson <llinharespinheiro@gmail.com>
2019-08-24 15:34:55 -0300

    cadastro e ajustes

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -37,4 +38,12 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
+    public function logged(){
+
+        if(Auth::check()){
+            return view('home');
+        }else{
+            return view('auth.login');
+        }
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -10,12 +10,10 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', 'Auth\LoginController@logged');
 
 Auth::routes();
 
